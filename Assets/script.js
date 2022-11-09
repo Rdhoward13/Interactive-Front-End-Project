9745f2f4139b650fbd3528e052405dca22eee8a5
ADeekollu <115489389+ADeekollu@users.noreply.github.com>
2022-11-09 14:47:58 -0500

    added second server side Api to top goal scorer

--- Assets/script.js
+++ Assets/script.js
@@ -2,7 +2,6 @@
 
 
 var title = document.getElementById("title")
-
 var card1 = document.querySelector(".card-1");
 var card2 = document.querySelector(".card-2");
 
@@ -39,28 +38,40 @@ function getGameApi() {
 getGameApi();
 
 
+function getTopGoalScorer() {
+	const options = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': '5c9e94c562msh1177d2f1faff343p1effe8jsnfd0a03ce8071',
+			'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+		}
+	};
+	
+	fetch('https://rjw-cors.herokuapp.com/https://api-football-v1.p.rapidapi.com/v3/players/topscorers?league=39&season=2022', options)
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(data){
+			console.log(data);
+
+			for (var i = 0; i < data.response.length; i++) {
+				// // var topScorer1 = document.createElement("h3")
+
+				// topScorer1.textContent = data[i].response[0].player.name
+
+				// card2.appendChild(topScorer1) ;
+				
+				console.log(data.response[i].player.name)
+				console.log(data.response[i].statistics[0].goals.total)
+			}
+	
+		})	
+		.catch(err => console.error(err));
+
+}
 
 
-
-// fetch("https://api.the-odds-api.com/v4/sports/soccer_epl/odds?apiKey=4294190c96a3485afbc5b3024f0fbdf6&regions=uk&markets=h2h")
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
-
-
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '5c9e94c562msh1177d2f1faff343p1effe8jsnfd0a03ce8071',
-		'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
-	}
-};
-
-fetch('https://rjw-cors.herokuapp.com/https://api-football-v1.p.rapidapi.com/v3/leagues?current=true', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+getTopGoalScorer();
 
 
 
